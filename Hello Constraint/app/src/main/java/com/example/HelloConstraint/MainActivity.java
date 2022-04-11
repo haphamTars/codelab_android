521d5d5a519620fc5dd1027d9ec068a34c308160
haphamTars <hagay0248@gmail.com>
2022-04-11 23:35:49 +0700

    change color

--- Hello Constraint/app/src/main/java/com/example/HelloConstraint/MainActivity.java
+++ Hello Constraint/app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -15,13 +15,25 @@ public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
     private Button countButton;
+    private Button zeroButton;
+    private Button toastButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+    }
+
+    public void init(){
         mShowCount = (TextView) findViewById(R.id.show_count);
         countButton = (Button) findViewById(R.id.button_count);
+        zeroButton = (Button) findViewById(R.id.button_zero);
+        toastButton = (Button) findViewById(R.id.button_toast);
+        if (mCount == 0){
+            zeroButton.setBackgroundColor(Color.GRAY);
+        }
     }
 
     public void showToast(View view) {
@@ -35,11 +47,18 @@ public class MainActivity extends AppCompatActivity {
         if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
         }
+        if(mCount%2 != 0)
+            countButton.setBackgroundColor(Color.RED);
+        else
+            countButton.setBackgroundColor(Color.GREEN);
 
+        zeroButton.setBackgroundColor(Color.BLUE);
     }
 
     public void zero(View view){
         mCount = 0;
         mShowCount.setText(Integer.toString(mCount));
+        zeroButton.setBackgroundColor(Color.GRAY);
+
     }
 }
