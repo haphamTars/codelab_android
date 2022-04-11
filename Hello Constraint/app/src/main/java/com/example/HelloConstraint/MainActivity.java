541dede135fe1bb272f6787013ca0e47895084e1
haphamTars <hagay0248@gmail.com>
2022-04-11 23:07:48 +0700

    1.2 B

---
+++ Hello Constraint/app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.HelloConstraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    private Button countButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        countButton = (Button) findViewById(R.id.button_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+    public void countUp(View view){
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+
+    }
+
+    public void zero(View view){
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+    }
+}
